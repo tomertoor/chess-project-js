b3cc1a5a6ac504fb0c3c612518e183e07f074668
tomertoor <tomertom150@gmail.com>
2022-02-06 23:27:59 +0200

    Last commit comment, mistake

--- chess/system-back-end/board.js
+++ chess/system-back-end/board.js
@@ -1,13 +1,50 @@
+import {Piece, Rook, Pawn, Knight, Queen, Bishop, King} from './pieces.js'
+
+const BOARD_SIZE = 10;
+
 class Board {
     #board;
 
     constructor(boardStr)
     {
-        
+        for(let i = 0; i < BOARD_SIZE; i++) {
+            switch(this.#board[i * BOARD_SIZE + i])
+            {
+                case 'B':
+                case 'b':
+                    this.#board[i * BOARD_SIZE + i] = new Bishop(this.#board[i * BOARD_SIZE + i], {x: i % BOARD_SIZE, y: (i - (i % BOARD_SIZE)) / BOARD_SIZE});
+                    break;
+                case 'R':
+                case 'r':
+                    this.#board[i * BOARD_SIZE + i] = new Rook(this.#board[i * BOARD_SIZE + i], {x: i % BOARD_SIZE, y: (i - (i % BOARD_SIZE)) / BOARD_SIZE});
+                    break;
+                case 'N':
+                case 'n':
+                    this.#board[i * BOARD_SIZE + i] = new Knight(this.#board[i * BOARD_SIZE + i], {x: i % BOARD_SIZE, y: (i - (i % BOARD_SIZE)) / BOARD_SIZE});
+                    break;
+                case 'B':
+                case 'b':
+                    this.#board[i * BOARD_SIZE + i] = new Bishop(this.#board[i * BOARD_SIZE + i], {x: i % BOARD_SIZE, y: (i - (i % BOARD_SIZE)) / BOARD_SIZE});
+                    break;
+                case 'Q':
+                case 'q':
+                    this.#board[i * BOARD_SIZE + i] = new Queen(this.#board[i * BOARD_SIZE + i], {x: i % BOARD_SIZE, y: (i - (i % BOARD_SIZE)) / BOARD_SIZE});
+                    break;
+                case 'K':
+                case 'k':
+                    this.#board[i * BOARD_SIZE + i] = new King(this.#board[i * BOARD_SIZE + i], {x: i % BOARD_SIZE, y: (i - (i % BOARD_SIZE)) / BOARD_SIZE});
+                    break;        
+            }
+        }
     }
 
-    makeBoard()
-    {
-        return ;
+    makeBoard() {
+        let boardStr = "";
+        for(let row in this.#board) {
+            for(let col in row) {
+                boardStr += col.getType();
+            }
+        }
+        return boardStr;
     }
 }
